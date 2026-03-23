1b875b24d52212dc97e36c679b3bf261111eecea
Alex Yang <2892565+aycyang@users.noreply.github.com>
2026-03-23 02:29:49 -0400

    test: Load reference images and run emulator
    Manually verified that emulator output is as expected

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import * as canvas from 'canvas'
 import * as escpos from 'escpos-ts'
 
 class Emulator {
-  canvas: Canvas | HTMLCanvasElement
+  canvas: HTMLCanvasElement
   private charMap: Image
   private strideX: number
   private strideY: number
   private cursorX: number
   private cursorY: number
   constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas ?? createCanvas(512, 256)
+    this.canvas = canvas
     this.strideX = 12
     this.strideY = 24
     this.cursorX = 0
@@ -22,11 +22,12 @@ class Emulator {
   }
   async init() {
     this.charMap = await loadImage(dataUrlA)
-    const ctx: any = this.canvas.getContext("2d")
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    const ctx = this.canvas.getContext("2d")
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
   }
   read(buf: Buffer) {
-    const ctx: any = this.canvas.getContext("2d")
+    const ctx = this.canvas.getContext("2d")
     const cmds = escpos.parse(buf)
     for (const cmd of cmds) {
       if (cmd instanceof escpos.InitializePrinter) {
@@ -43,7 +44,7 @@ class Emulator {
             const y = Math.floor(c / 16) - 2
             const sx = this.strideX * x
             const sy = this.strideY * y
-            ctx.drawImage(this.charMap,
+            ctx.drawImage(this.charMap as any,
               sx, sy, this.strideX, this.strideY,
               this.cursorX, this.cursorY, this.strideX, this.strideY)
             this.cursorX += this.strideX
--- test/all.js
+++ test/all.js
@@ -3,20 +3,48 @@ import assert from "node:assert"
 import fs from "node:fs"
 import { join } from "node:path"
 import { Emulator } from "./dist/main.js"
+import { Buffer } from "buffer"
 import { createCanvas, loadImage } from "canvas"
+
+function parseHex(hex) {
+  hex = hex.replace(/[ \t\r\n]/g, '')
+  const buf = Buffer.from(hex, 'hex')
+  return buf
+}
 
 const testDataPath = "test/data"
 for (const entry of fs.readdirSync(testDataPath, { withFileTypes: true })) {
   if (entry.isDirectory()) {
     test(entry.name, async () => {
-      const inPath = join(testDataPath, entry.name, "in.txt")
-      const refImgPath = join(testDataPath, entry.name, "ref.jpg")
+      const refData = await (async () => {
+        const path = join(testDataPath, entry.name, "ref.jpg")
+        const img = await loadImage(path)
+        const canvas = createCanvas(img.width, img.height)
+        const ctx = canvas.getContext("2d")
+        ctx.drawImage(img, 0, 0)
+        return ctx.getImageData(0, 0, canvas.width, canvas.height)
+      })()
 
-      const testInput = fs.readFileSync(inPath)
-      const refImg = await loadImage(refImgPath)
+      const actualData = await (async () => {
+        const path = join(testDataPath, entry.name, "in.txt")
+        const inputHexString = fs.readFileSync(path, { encoding: "utf8" })
+        const inputBytes = parseHex(inputHexString)
+        const canvas = createCanvas(512, 256)
+        const emulator = new Emulator(canvas)
+        await emulator.init()
+        emulator.read(inputBytes)
+        const ctx = canvas.getContext("2d")
 
-      const emuCanvas = createCanvas()
-      const emu = new Emulator(emuCanvas)
+        // write the canvas output to a file
+        //const buf = canvas.toBuffer("image/png")
+        //const outPath = join(testDataPath, entry.name, "actual.png")
+        //fs.writeFileSync(outPath, buf)
+
+        return ctx.getImageData(0, 0, canvas.width, canvas.height)
+      })()
+
+      console.log("ref", refData.width, refData.height)
+      console.log("actual", actualData.width, actualData.height)
     })
   }
 }
