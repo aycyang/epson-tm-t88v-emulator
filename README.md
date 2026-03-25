1bccb7a2f00dd5d6b96794c05055ac1d5ff32438
Alex Yang <2892565+aycyang@users.noreply.github.com>
2026-03-24 21:52:36 -0400

    Minimum viable visual diffing test infra

---
+++ README.md
@@ -0,0 +1,11 @@
+# Epson TM-T88V Emulator
+
+### Tests
+
+To add a new test:
+
+1. Create a new directory in `test/data`. Give the directory a name that describes the test.
+1. Come up with some ESC/POS that demonstrates a specific feature of the receipt printer. Put that in `in.txt` as hexadecimal.
+1. This is not strictly necessary for the test to work, but in case of improvements to the text data pipeline, put the original scanned image of the receipt in the directory (e.g. `scan.jpg`.
+1. Use [deskew-text](https://github.com/aycyang/deskew-text) to generate `ref.png` from `scan.jpg`. It should have straightened and thresholded (made pure black-and-white) `scan.jpg`.
+1. Now when you run `npm test`, it should detect the new directory you created and generate `diff.png`, which you can then visually inspect for differences between the emulator output and the real-life output.
--- test/all.js
+++ test/all.js
@@ -1,10 +1,10 @@
 import test from "node:test"
 import assert from "node:assert"
 import fs from "node:fs"
-import * as path from "node:path"
+import path from "node:path"
 import { Emulator } from "./dist/main.js"
 import { Buffer } from "buffer"
-import { createCanvas, loadImage } from "canvas"
+import { createImageData, createCanvas, loadImage } from "canvas"
 
 const kTestDataPath = "test/data"
 
@@ -22,11 +22,123 @@ function writeImageDataToFile(imageData, outPath) {
   fs.writeFileSync(outPath, buf)
 }
 
+function crop(src, x, y, w, h) {
+  const dst = createImageData(w, h)
+  for (let i = 0; i < w; i++) {
+    const xx = x + i
+    if (xx >= src.width) continue
+    for (let j = 0; j < h; j++) {
+      const yy = y + j
+      if (yy >= src.height) continue
+      const srcIdx = yy * src.width + xx
+      const dstIdx = j * dst.width + i
+      dst.data[dstIdx * 4 + 0] = src.data[srcIdx * 4 + 0]
+      dst.data[dstIdx * 4 + 1] = src.data[srcIdx * 4 + 1]
+      dst.data[dstIdx * 4 + 2] = src.data[srcIdx * 4 + 2]
+      dst.data[dstIdx * 4 + 3] = src.data[srcIdx * 4 + 3]
+    }
+  }
+  return dst
+}
+
+function diffImageData(a, b, offsetX, offsetY, scale) {
+  const diffData = createImageData(a.width, a.height)
+
+  for (let ay = 0; ay < a.height; ay++) {
+    const by = Math.round((ay - offsetY) / scale)
+    for (let ax = 0; ax < a.width; ax++) {
+      const bx = Math.round((ax - offsetX) / scale)
+
+      const ai = ay * a.width + ax
+      const bi = by * b.width + bx
+
+      const av = a.data[ai * 4]
+      let bv = 255
+      if (0 <= by && by < b.height && 0 <= bx && bx < b.width) {
+        bv = b.data[bi * 4]
+      }
+
+      if (av === 0 && bv === 0) {
+        diffData.data[ai * 4 + 0] = 0
+        diffData.data[ai * 4 + 1] = 255
+        diffData.data[ai * 4 + 2] = 0
+        diffData.data[ai * 4 + 3] = 255
+      } else if (av === 255 && bv === 0) {
+        diffData.data[ai * 4 + 0] = 255
+        diffData.data[ai * 4 + 1] = 0
+        diffData.data[ai * 4 + 2] = 0
+        diffData.data[ai * 4 + 3] = 255
+      } else if (av === 0 && bv === 255) {
+        diffData.data[ai * 4 + 0] = 0
+        diffData.data[ai * 4 + 1] = 0
+        diffData.data[ai * 4 + 2] = 255
+        diffData.data[ai * 4 + 3] = 255
+      } else if (av === 255 && bv === 255) {
+        diffData.data[ai * 4 + 0] = 255
+        diffData.data[ai * 4 + 1] = 255
+        diffData.data[ai * 4 + 2] = 255
+        diffData.data[ai * 4 + 3] = 255
+      } else {
+        // unexpected error; paint magenta
+        diffData.data[ai * 4 + 0] = 255
+        diffData.data[ai * 4 + 1] = 0
+        diffData.data[ai * 4 + 2] = 255
+        diffData.data[ai * 4 + 3] = 255
+      }
+    }
+  }
+
+  return diffData
+}
+
+function calculateAlignmentScore(a, b, offsetX, offsetY, scale) {
+  let correct = 0
+  let incorrect = 0
+  for (let ay = 0; ay < a.height; ay++) {
+    const by = Math.round((ay - offsetY) / scale)
+    for (let ax = 0; ax < a.width; ax++) {
+      const bx = Math.round((ax - offsetX) / scale)
+
+      const ai = ay * a.width + ax
+      const bi = by * b.width + bx
+
+      const av = a.data[ai * 4]
+      let bv = 255
+      if (0 <= by && by < b.height && 0 <= bx && bx < b.width) {
+        bv = b.data[bi * 4]
+      }
+
+      if (av === 0 && bv === 0) {
+        correct++
+      } else if (av === 255 && bv === 0 || av === 0 && bv === 255) {
+        incorrect++
+      }
+    }
+  }
+  return correct / (correct + incorrect)
+}
+
+function calculateOptimalTransform(a, b, kernelWidth, kernelHeight, scaleStart, scaleEnd, scaleStep) {
+  const hkw = Math.trunc(kernelWidth / 2)
+  const hkh = Math.trunc(kernelHeight / 2)
+  const alignmentScores = []
+  for (let s = scaleStart; s < scaleEnd; s += scaleStep) {
+    for (let i = -hkw; i < kernelWidth - hkw; i++) {
+      for (let j = -hkh; j < kernelHeight - hkh; j++) {
+        const score = calculateAlignmentScore(a, b, i, j, s)
+        alignmentScores.push([score, i, j, s])
+      }
+    }
+  }
+  alignmentScores.sort().reverse()
+  return alignmentScores[0]
+}
+
 for (const entry of fs.readdirSync(kTestDataPath, { withFileTypes: true })) {
   if (entry.isDirectory()) {
     test(entry.name, async () => {
       const refData = await (async () => {
-        const inPath = path.join(kTestDataPath, entry.name, "ref.jpg")
+        const inPath = path.join(kTestDataPath, entry.name, "ref.png")
         const img = await loadImage(inPath)
         const canvas = createCanvas(img.width, img.height)
         const ctx = canvas.getContext("2d")
@@ -43,12 +155,6 @@ for (const entry of fs.readdirSync(kTestDataPath, { withFileTypes: true })) {
         await emulator.init()
         emulator.read(inputBytes)
         const ctx = canvas.getContext("2d")
-
-        // write the canvas output to a file
-        //const buf = canvas.toBuffer("image/png")
-        //const outPath = path.join(kTestDataPath, entry.name, "actual.png")
-        //fs.writeFileSync(outPath, buf)
-
         return ctx.getImageData(0, 0, canvas.width, canvas.height)
       })()
 
@@ -69,44 +175,18 @@ for (const entry of fs.readdirSync(kTestDataPath, { withFileTypes: true })) {
         refData.data[i+3] = 255
       }
 
-      console.log(actualData.width * actualData.height * 4, actualData.data.length)
+      // Approximate alignment using a patch near the top left of both images
+      const refPatch = crop(refData, 0, 0, 50, 50)
+      const actualPatch = crop(actualData, 0, 0, 25, 25)
+      // Scale is hardcoded at 1.68 for the HP scanner I'm using.
+      // The scale-space search is not robust enough as is.
+      // TODO Should determine scale automatically for more flexibility.
+      const [score, offsetX, offsetY, scale] = calculateOptimalTransform(refPatch, actualPatch, 7, 7, 1.68, 1.69, 0.1)
 
-      writeImageDataToFile(refData, path.join(kTestDataPath, entry.name, "tmp.png"))
+      const diffData = diffImageData(refData, actualData, offsetX, offsetY, scale)
+      writeImageDataToFile(diffData, path.join(kTestDataPath, entry.name, "diff.png"))
 
-      for (let y = 0; y < refData.height && y < actualData.height; y++) {
-        for (let x = 0; x < refData.width && x < actualData.width; x++) {
-          const refIdx = y * refData.width + x
-          const actualIdx = y * actualData.width + x
-          const refValue = refData.data[refIdx * 4 + 0]
-          const actualValue = actualData.data[actualIdx * 4 + 0]
-          let r = 255
-          let g = 255
-          let b = 255
-          if (refValue === 0 && actualValue === 0) { // both black; correct
-            r = 0
-            g = 255
-            b = 0
-          } else if (refValue === 0 && actualValue === 255) { // was white, expected black
-            r = 0
-            g = 0
-            b = 255
-          } else if (refValue === 255 && actualValue === 0) { // was black, expected white
-            r = 255
-            g = 0
-            b = 0
-          } else {
-            r = actualValue
-            g = actualValue
-            b = actualValue
-          }
-          refData.data[refIdx * 4 + 0] = r
-          refData.data[refIdx * 4 + 1] = g
-          refData.data[refIdx * 4 + 2] = b
-          refData.data[refIdx * 4 + 3] = 255
-        }
-      }
-
-      writeImageDataToFile(refData, path.join(kTestDataPath, entry.name, "tmp2.png"))
+      assert(score >= 0.8, `match score is less than 80%: ${score}`)
     })
   }
 }
